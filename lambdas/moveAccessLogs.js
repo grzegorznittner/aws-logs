6d12891e046d27f391b5d63ab5f681577712e1fb
Grzegorz Nittner <grzegorz.nittner@gmail.com>
2020-02-07 20:53:02 +0100

    Log move lambda function with tests

---
+++ lambdas/moveAccessLogs.js
@@ -0,0 +1,102 @@
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+const targetBucket = process.env.TARGET_BUCKET;
+const DEBUG = process.env.DEBUG;
+
+exports.handler = async (event, context, callback) => {
+  const moves = event.Records.map(record => {
+    const bucket = record.s3.bucket.name;
+    const sourceKey = record.s3.object.key;
+    console.log(`Handling s3://${bucket}/${sourceKey}.`);
+
+    const copyParams = exports.createCopyParams(bucket, sourceKey, targetBucket);
+    const deleteParams = { Bucket: bucket, Key: sourceKey };
+    const copy = s3.copyObject(copyParams).promise();
+
+    return copy.then(function () {
+        console.log(`Copied s3://${bucket}/${sourceKey} to s3://${copyParams.Bucket}/${copyParams.Key}`);
+        const del = s3.deleteObject(deleteParams).promise();
+        console.log(`Deleted ${sourceKey}.`);
+        return del;
+      }, function (reason) {
+        var error = new Error(`Error while copying s3://${bucket}/${sourceKey}: ${reason}`);
+        callback(error);
+      });
+  });
+  await Promise.all(moves);
+};
+
+
+exports.createCopyParams = (sourceBucket, sourceKey, targetBucket) => {
+  const cloudfrontRegex = /^cloudfronts\/(?<prefix>[^.]*).(?<year>\d*)-(?<month>\d*)-(?<day>\d*)-(?<hour>\d*).(?<suffix>.*)/gm;
+  const s3accessLogsRegex = /^s3buckets\/(?<year>\d*)-(?<month>\d*)-(?<day>\d*)-(?<hour>\d*).(?<suffix>.*)/gm;
+  const cloudtrailDigestRegex = /^cloudtrail\/AWSLogs\/(?<account>[^\/]*)\/CloudTrail-Digest\/(?<region>[^\/]*)\/(?<year>\d*)\/(?<month>\d*)\/(?<day>\d*)\/(?<suffix>.*)\d{8}T(?<hour>\d{2}).*/gm;
+  const cloudtrailRegex = /^cloudtrail\/AWSLogs\/(?<account>[^\/]*)\/CloudTrail\/(?<region>[^\/]*)\/(?<year>\d*)\/(?<month>\d*)\/(?<day>\d*)\/(?<suffix>.*)\d{8}T(?<hour>\d{2}).*/gm;
+
+  console.log(`source key: ${sourceKey}`);
+  let targetKey;
+  let m;
+  if ((m = cloudfrontRegex.exec(sourceKey)) !== null) {
+    targetKey = handleCloudFrontLogs(m, cloudfrontRegex);
+  } else if ((m = s3accessLogsRegex.exec(sourceKey)) !== null) {
+    targetKey = handleS3AccessLogs(m, s3accessLogsRegex);
+  } else if ((m = cloudtrailRegex.exec(sourceKey)) !== null) {
+    targetKey = handleCloudTrailLogs(sourceKey, m, cloudtrailRegex);
+  } else if ((m = cloudtrailDigestRegex.exec(sourceKey)) !== null) {
+    targetKey = handleCloudTrailDigestLogs(sourceKey, m, cloudtrailDigestRegex);
+  } else {
+    targetKey = handleUnrecognized(sourceKey);
+  }
+
+  return {
+    CopySource: sourceBucket + '/' + sourceKey,
+    Bucket: targetBucket,
+    Key: targetKey
+  };
+};
+
+function handleCloudFrontLogs(m, regex) {
+  console.log(`handleCloudFrontLogs`);
+  printMatches(m, regex);
+  return `cloudfront/raw/year=${m[2]}/month=${m[3]}/day=${m[4]}/hour=${m[5]}/${m[1]}.${m[2]}-${m[3]}-${m[4]}-${m[5]}.${m[6]}`;
+}
+
+function handleS3AccessLogs(m, regex) {
+  console.log(`handleS3AccessLogs`);
+  printMatches(m, regex);
+  return `s3buckets/raw/year=${m[1]}/month=${m[2]}/day=${m[3]}/hour=${m[4]}/${m[1]}-${m[2]}-${m[3]}-${m[4]}-${m[5]}`;
+}
+
+function handleCloudTrailLogs(sourceKey, m, regex) {
+  console.log(`handleCloudTrailLogs`);
+  printMatches(m, regex);
+  const filename = sourceKey.split('/').slice(-1).pop();
+  return `cloudtrail/raw/year=${m[3]}/month=${m[4]}/day=${m[5]}/hour=${m[7]}/${filename}`;
+}
+
+function handleCloudTrailDigestLogs(sourceKey, m, regex) {
+  console.log(`handleCloudTrailDigestLogs`);
+  printMatches(m, regex);
+  const filename = sourceKey.split('/').slice(-1).pop();
+  return `cloudtrail/digest/year=${m[3]}/month=${m[4]}/day=${m[5]}/hour=${m[7]}/${filename}`;
+}
+
+function handleUnrecognized(sourceKey) {
+  console.log(`Found unrecognized: ${sourceKey}`);
+  const filename = sourceKey.split('/').slice(-1).pop();
+  return `unrecognized/${filename}`;
+}
+
+function printMatches(m, regex) {
+  if ( typeof DEBUG !== 'undefined' && DEBUG ) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+    // The result can be accessed through the `m`-variable.
+    m.forEach((match, groupIndex) => {
+        console.log(`  - ${groupIndex}: ${match}`);
+    });
+  }
+}
---
+++ test/moveAccessLogsTest.js
@@ -0,0 +1,124 @@
+var { createCopyParams }  = require("../lambdas/moveAccessLogs");
+var assert = require("assert");
+
+describe("analytics move access logs function - createCopyParams tests", function() {
+  it("cloudfront logs 1", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "cloudfronts/E2RM06AWPLPG4A.2019-12-23-04.ee0a91d6.gz";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "cloudfront/raw/year=2019/month=12/day=23/hour=04/E2RM06AWPLPG4A.2019-12-23-04.ee0a91d6.gz");
+  });
+
+  it("cloudfront logs 2", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "cloudfronts/E2MB6G0UQ7QSIX.2019-11-25-05.1f5f36ff.gz";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "cloudfront/raw/year=2019/month=11/day=25/hour=05/E2MB6G0UQ7QSIX.2019-11-25-05.1f5f36ff.gz");
+  });
+  
+  it("cloudfront logs 3", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "cloudfronts/EW0CYG3RXMS96.2019-12-11-09.cdcf4ab4.gz";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "cloudfront/raw/year=2019/month=12/day=11/hour=09/EW0CYG3RXMS96.2019-12-11-09.cdcf4ab4.gz");
+  });
+  
+  it("cloudfront logs 4", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "cloudfronts/E2NJ3QYW5Z28DZ.2019-11-14-19.c1d8d31e.gz";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "cloudfront/raw/year=2019/month=11/day=14/hour=19/E2NJ3QYW5Z28DZ.2019-11-14-19.c1d8d31e.gz");
+  });
+  
+  it("s3 bucket access logs 1", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "s3buckets/2019-10-07-07-34-53-96349443E23AFB45";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "s3buckets/raw/year=2019/month=10/day=07/hour=07/2019-10-07-07-34-53-96349443E23AFB45");
+  });
+  
+  it("s3 bucket access logs 2", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "s3buckets/2019-12-18-14-18-13-5274284401078609";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "s3buckets/raw/year=2019/month=12/day=18/hour=14/2019-12-18-14-18-13-5274284401078609");
+  });
+
+  it("cloud trail logs", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "cloudtrail/AWSLogs/1234567890/CloudTrail/eu-central-1/2019/12/06/1234567890_CloudTrail_eu-central-1_20191206T0000Z_ogS3SY6UQgeVHcJc.json.gz";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "cloudtrail/raw/year=2019/month=12/day=06/hour=00/1234567890_CloudTrail_eu-central-1_20191206T0000Z_ogS3SY6UQgeVHcJc.json.gz");
+  });
+  
+  it("cloud trail digest logs", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "cloudtrail/AWSLogs/1234567890/CloudTrail-Digest/eu-central-1/2019/11/02/1234567890_CloudTrail-Digest_eu-central-1_preprod-trail_eu-central-1_20191102T003813Z.json.gz";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "cloudtrail/digest/year=2019/month=11/day=02/hour=00/1234567890_CloudTrail-Digest_eu-central-1_preprod-trail_eu-central-1_20191102T003813Z.json.gz");
+  });
+  
+  it("unrecognized logs 1", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "cloudtrail/1234567890_CloudTrail-Digest_eu-central-1_preprod-trail_eu-central-1_20191102T003813Z.json.gz";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "unrecognized/1234567890_CloudTrail-Digest_eu-central-1_preprod-trail_eu-central-1_20191102T003813Z.json.gz");
+  });
+  
+  it("unrecognized logs 2", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "abc/1234567890_CloudTrail-Digest_eu-central-1_preprod-trail_eu-central-1_20191102T003813Z.json.gz";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "unrecognized/1234567890_CloudTrail-Digest_eu-central-1_preprod-trail_eu-central-1_20191102T003813Z.json.gz");
+  });
+  
+  it("unrecognized logs 3", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "abc/def/1234567890_CloudTrail-Digest_eu-central-1_preprod-trail_eu-central-1_20191102T003813Z.json.gz";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "unrecognized/1234567890_CloudTrail-Digest_eu-central-1_preprod-trail_eu-central-1_20191102T003813Z.json.gz");
+  });
+  
+  it("unrecognized logs 4", function() {
+    const sourceBucket = "source-bucket";
+    const sourceKey = "1234567890_CloudTrail-Digest_eu-central-1_preprod-trail_eu-central-1_20191102T003813Z.json.gz";
+    const targetBucket = "target-bucket";
+    const output = createCopyParams(sourceBucket, sourceKey, targetBucket);
+    assert.equal(output.CopySource, sourceBucket + "/" + sourceKey);
+    assert.equal(output.Bucket, targetBucket);
+    assert.equal(output.Key, "unrecognized/1234567890_CloudTrail-Digest_eu-central-1_preprod-trail_eu-central-1_20191102T003813Z.json.gz");
+  });
+});
